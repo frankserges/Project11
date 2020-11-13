91fe5dd9dcafd420637f5739aa7432936822fa0a
frank Ndjekoua <franckndjekoua@yahoo.fr>
2020-11-13 13:11:14 +0100

    second commit

--- C_Coding_Standard.c
+++ C_Coding_Standard.c
@@ -1,13 +1,23 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include<stddef.h>
 
 
-#define assign(uc1, uc2, val) uc1##uc2 = val
+/******************************************************************************
+PRE31-C. Avoid side effects in arguments to unsafe macros
+******************************************************************************/
 
-int main(void) {
-	int \u0401;
-	/* ... */
-	assign(\u04, 01, 4);
-	printf("hello");
+/******************************************************************************
+Noncompliant Code Example
+******************************************************************************/
+
+
+#define abs(x) (((x) < 0) ? -(x) : (x))
+
+int main(int n) {
+	/* validate that n is within the desired range */
+	int m = abs(++n);
+	printf("m=%d \n", m);
+
 	return EXIT_SUCCESS;
 }
